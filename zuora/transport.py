0c5165bdddbce057cc4777d91cb1e4fc661b5925
Fantomas42 <fantomas42@gmail.com>
2014-02-06 15:56:54 +0100

    Set timeout to 20 seconds

--- zuora/transport.py
+++ zuora/transport.py
@@ -12,7 +12,7 @@ class HttpTransportWithKeepAlive(HttpAuthenticated, object):
 
     def __init__(self):
         super(HttpTransportWithKeepAlive, self).__init__()
-        self.http = httplib2.Http(timeout=5)
+        self.http = httplib2.Http(timeout=20)
 
     def open(self, request):
         return HttpTransport.open(self, request)
